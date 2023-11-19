7b4e4c68779b5b0118566a44c529dca902ab6fe6
Shalaby-VBS <shalaby.vbs@gmail.com>
2023-11-19 14:02:35 +0200

    ✏️ Edit MaterialApp title.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Advanced',
+      title: 'Flutter Advanced Course',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),

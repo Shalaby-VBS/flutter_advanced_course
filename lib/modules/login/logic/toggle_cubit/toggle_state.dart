06319a0605a7f32c8977abfa1cbd7205f93fe603
Shalaby-VBS <shalaby.vbs@gmail.com>
2023-12-12 09:51:30 +0200

    - Added Spacing File For SizedBox. - Added Some Colors & TextStyles. - Added FontWeight Helper. - Added General TextButton & General TextFormField. - Finished Login Screen Design As UI.

---
+++ lib/modules/login/logic/toggle_cubit/toggle_state.dart
@@ -0,0 +1,14 @@
+part of 'toggle_cubit.dart';
+
+class ToggleState {
+  final bool value;
+
+  const ToggleState(this.value);
+}
+
+class RememberMeState {
+  final bool value;
+
+  const RememberMeState(this.value);
+}
+
